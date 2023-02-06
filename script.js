aa12ed1349663d20e756e4f7ed1f0ae297702b25
MANISH KUMAR <117758302+Manishkumar76@users.noreply.github.com>
2023-02-06 12:10:19 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,55 @@
+let joke=[`ਕਹਿੰਦੀ ਮੇਰਾ Phone ਮੇਰੀ ਮੰਮੀ ਕੋਲ ਹੁੰਦਾ ਜਿਆਦਾਤਰ
+ਮੈਂ ਕਿਹਾ ਸਾਲੀਏ ਫੜ ਹੋ ਜਾਵਾਗੇ ਫਿਰ ਤਾਂ ?
+ਕਹਿੰਦੀ ਨਹੀ ਫੜ ਹੋਣਾ !!!
+ਮੈਂ ਕਿਹਾ ਓਹ ਕਿਦਾਂ ?
+ਕਹਿੰਦੀ ਤੇਰਾ ਨੰਬਰ ਮੈਂ Low Battery ਲਿਖ ਕੇ Save ਕੀਤਾ ਆ� ਜਦੋ ਤੇਰੀ Call ਆਉਂਦੀ ਮੰਮੀ ਚਾਰਜ ਤੇ ਲਾ ਦਿੰਦੀ ਆ�!!!`,`IK Munda Pepsi Samne Rakh Ke Udas Betha C..
+
+Dost aaya Pepsi Pee Giya Te Puchiya
+Yaar tu Udaas Kyo a......??
+Dost Ne Kiha Yaar Aaj da Din e Kharab C
+Swere GF naal ladaye Ho gye..
+Raste vich Car Kharab Ho gye..
+0ffice Late Pounchiya te Boss ne Naukri ton Kad ta..
+Hun Suicide Krn Laye Pepsi vich ZAHER Paya c... 
+Ohvi Tu pe giya....`,`ਸ਼ਰਾਬੀ ਬੀਅਰ ਬਾਰ ਵਾਲੇ ਨੂੰ ਕਹਿੰਦਾ peg ਬਣਾ ਲੜਾਈ ਹੋਣ ਵਾਲੀ ਏ
+ਫਿਰ ਕਹਿੰਦਾ .. .?
+ਇਕ ਹੋਰ peg ਬਣਾ ਲੜਾਈ ਹੋਣ ਵਾਲੀ ਏ ..
+ਬੀਅਰ ਬ਼ਾਰ ਵਾਲਾ ਕਹਿੰਦਾ:- Sir ਲੜਾਈ ਹੁਣੀ ਕਦੋ ਏ ?????
+ਕਹਿੰਦਾ ਜਦੋ ਤੂੰ ਸਾਲਿਆ peg ਦੇ ਪੈਸੇ ਮੰਗੇਗਾ`,`ਮੁੰਡਾ ਕੁੜੀ ਨੂੰ ਫੋਨ ਕਰਦਾ �ਕੀ ਕਰਦੀ ਏ ਜਾਨ ?
+ਕੁੜੀ :- ਘਰ ਆ ਸੋਣ ਲੱਗੀ ਆ ਸਿਰ ਦਰਦ ਬਹੁਤ ਹੋ ਰਿਹਾ , ਤੂੰ ਕੀ ਕਰ ਰਿਹਾ ਏ ?
+ਮੁੰਡਾ :- ਮੈ ਸਿਨੇਮਾ ਹਾਲ ਵਿਚ ਤੇਰੀ ਪਿੱਛਲੀ ਸੀਟ ਤੇ ਬੇਠਾ ਫੁੱਲੇ ਖਾ ਰਿਹਾ ਕਮੀਨੀਏ`,`ਮੁੰਡਾ ਕੁੜੀ ਨੂੰ:� ਚਲਤੀ ਹੈ ਕਿਆ 9 ਸੇ 12,
+ਕੁੜੀ:- ਚੱਲ?
+ਮੁੰਡਾ:- ਕਿੱਥੇ ??
+ਕੁੜੀ:- ਪ੍ਰਿੰਸੀਪਲ ਕੋਲ
+ਮੁੰਡਾ:- ਲੈ ਦੱਸ �ਹੁਣ ਅਸੀਂ ਭੈਣ ਨਾਲ ਮਜ਼ਾਕ ਵੀ ਨੀ ਕਰ ਸਕਦੇ?�
+ਕੁੜੀ:- ਪਾਗ਼ਲਾ ਮੈ ਛੁੱਟੀ ਲੈਣ ਵਾਸਤੇ ਕਹਿੰਦੀ ਪਈ ਆ`,`
+ਕੁੜੀ - ਜਾਨੂੰ ਇੱਕ ਪੱਪੀ ਤਾਂ ਦੇਦੋ .. :
+ਮੁੰਡਾ - ਸਾਡੇ ਮੁਹੱਲੇ ਚ ਕੁੱਤੀ ਨੇ 8-10 ਦਿੱਤੇ ਹੋਏ ਨੇ .....
+ਆਕੇ ਲੈਜਾਂ ..?
+....
+ਮੈਨੂੰ ਤਾਂ ਕੁੱਤੀ ਖਾਣ ਨੂੰ ਪੈਂਦੀ ਆਂ.. :P
+...
+ਕੁੜੀ - ਕੱਟੀ :/
+..
+ਮੁੰਡਾ - ਸਾਡੇ ਪਿੰਡ ਚ ਮੱਝ ਨੇ ਦਿੱਤੀ ਆਂ 
+ਇੱਕ ਕੱਟੀ ਆਕੇ ਲੈਜਾਂ, ਮੈਨੂੰ ਤਾਂ ਮੱਝ ਵੀ ਖਾਣ ਨੂੰ ਪੈਂਦੀ ਆਂ`,`ਜਨਤਾ ਵੀ ਕਮਾਲ ਕਰਦੀ ਆ ,
+ਅੱਜ ਇਕ ਬੰਦਾ ਮੂੰਹ ਤੇ ਹੱਥ ਰੱਖੀ ਆਵੇ , ਸੱਜਾ ਪਾਸਾ ਦੱਬੀ ।
+ਕਿਸੇ ਨੇ ਪੁੱਛ ਲਿਆ ਵੀ ਕੀ ਹੋ ਗਿਆ
+ਕਹਿੰਦਾ ਜਾੜ ਨੂੰ ਕੀੜਾ ਲੱਗਿਆ ਹੋਇਆ , ਦੰਦਾਂ ਦੇ ਡਾਕਟਰ ਕੋਲ ਚਲੀਆਂ , ਜਾੜ ਕਢਵਾ ਦੇਣੀ ਆ ।
+ਇੰਨੇ ਨੂੰ ਜਾਗਰ ਸਿੰਘ ਕੋਲੋ ਲੰਗਿਅਾ ਜਾਂਦਾ ਬੋਲਿਆ ,
+ਕਾਹਨੂੰ ਪੈਸੇ ਖ਼ਰਾਬ ਕਰਦਾ ਡਾਕਟਰ ਤੇ , 
+ਮਾਰ ਇਕ ਨਾਅਰਾ ,
+ਅਕਾਲੀ ਦਲ ਜ਼ਿੰਦਾਵਾਦ ।
+ਕੋਈ ਮੁਫ਼ਤ ਚ ਕੱਢ ਦੂ .`,
+`Wife: Meri Umar 45 Hon De Baawjood Vi Tuhada Ik Dost Meri Husan Di Taareef Karda Ni Thakda�
+Husband: Ik Hi Banda Kar Sakda E, Oh Maange Raam? Haina?
+Wife: Oh God, Sahi Keha, Tuanu Kive Pta�
+Husband: Kabaadi E Na�`];
+
+let c=joke[Math.floor(Math.random()*joke.length)];
+console.log(c);
+  
+
+let but=document.getElementById('button1').onclick=()=>{
+document.getElementById('joke').innerHTML = c;
+document.getElementById('joke').style.fontsize="large";}
